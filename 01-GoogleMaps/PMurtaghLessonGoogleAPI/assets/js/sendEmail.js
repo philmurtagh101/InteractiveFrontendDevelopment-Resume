a9b62ebb18eb4f8fa4485e6f8e2edd690558b002
philmurtagh101 <philmurtagh101@gmail.com>
2021-06-14 19:28:52 +0000

    Email leveraging EMAILJS is working

--- 01-GoogleMaps/PMurtaghLessonGoogleAPI/assets/js/sendEmail.js
+++ 01-GoogleMaps/PMurtaghLessonGoogleAPI/assets/js/sendEmail.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+function sendMail(contactForm){
+    //first argument is the service and second is the template
+    //email.js is a promise
+    emailjs.send("service_6ilt0wn","rosie_JStest_101",{
+        "from_name":contactForm.name.value, 
+        "from_email":contactForm.emailaddress.value,
+        "project_request":contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log("SUCCESS", response);
+        },
+        function(error){
+            console.log("FAILED", error);
+        });
+
+        return false;
+}
